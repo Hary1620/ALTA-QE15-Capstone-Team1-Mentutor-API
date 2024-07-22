6c4b283792ef13f96466f9c804217b8f00c92a2e
Tonaas Kalesaran <tonaaskalesaranon@gmail.com>
2024-07-22 19:29:45 +0700

    validate schema failed, negative test failed

--- src/test/java/starter/Forum/ForumAPI.java
+++ src/test/java/starter/Forum/ForumAPI.java
@@ -1,14 +1,18 @@
 package starter.Forum;
 
+import io.restassured.http.ContentType;
 import net.serenitybdd.rest.SerenityRest;
 import net.thucydides.core.annotations.Step;
 import net.thucydides.core.annotations.Steps;
 import starter.Login.LoginAPI;
 import starter.utils.Constants;
 
+import java.io.File;
+
 public class ForumAPI {
 
     public static String GET_ALL_STATUS = Constants.BASE_URL+ "/forum";
+    public static String NEW_STATUS = Constants.BASE_URL+ "/forum";
 
     @Steps
     LoginAPI loginAPI;
@@ -17,5 +21,13 @@ public class ForumAPI {
     public void getAllStatus(){
         SerenityRest.given()
                 .header("Authorization", "Bearer " + loginAPI.TOKEN_MENTEE);
+    }
+
+    @Step("Create new status")
+    public void createNewStatus(File json){
+        SerenityRest.given()
+                .header("Authorization", "Bearer " + loginAPI.TOKEN_MENTEE)
+                .contentType(ContentType.JSON)
+                .body(json);
     }
 }
--- src/test/java/starter/Forum/stepdef/GetAllStatusForumStepdef.java
+++ src/test/java/starter/Forum/stepdef/GetAllStatusForumStepdef.java
@@ -1,10 +1,16 @@
 package starter.Forum.stepdef;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.module.jsv.JsonSchemaValidator;
 import net.serenitybdd.rest.SerenityRest;
 import net.thucydides.core.annotations.Steps;
 import starter.Forum.ForumAPI;
+import starter.utils.Constants;
+
+import java.io.File;
 
 public class GetAllStatusForumStepdef {
     @Steps
@@ -17,7 +23,24 @@ public class GetAllStatusForumStepdef {
 
     @When("Send request get all status")
     public void sendRequestGetAllStatus() {
-        SerenityRest.when().get(ForumAPI.GET_ALL_STATUS);
+        SerenityRest.when().get(forumAPI.GET_ALL_STATUS);
     }
+
+    @Given("Add status on forum with data {string}")
+    public void addStatusOnForumWithData(String file) {
+        File fileJson = new File(Constants.REQ_BODY+file);
+        forumAPI.createNewStatus(fileJson);
+    }
+
+    @When("Send request add status")
+    public void sendRequestAddStatus() {
+        SerenityRest.when().get(forumAPI.NEW_STATUS);
+    }
+
+    @And("Validate json schema {string}")
+    public void validateJsonSchema(String fileName) {
+            File jsonFile = new File(Constants.JSON_SCHEMA+fileName);
+            SerenityRest.and().body(JsonSchemaValidator.matchesJsonSchema(jsonFile));
+        }
 
 }
